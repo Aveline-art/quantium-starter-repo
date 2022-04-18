57dfe11ccf1f1b9e6abe9c08591cc9c80b1682e0
avali4907 <avali4907@outlook.com>
2022-04-17 17:29:40 -0700

    testing tutorial sample

--- app.py
+++ app.py
@@ -5,6 +5,36 @@ from dash import Dash, dcc, html
 import plotly.express as px
 import pandas as pd
 
+from dash import Dash, dcc, html, Input, Output
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.H6("Change the value in the text box to see callbacks in action!"),
+    html.Div([
+        "Input: ",
+        dcc.Input(id='my-input', value='initial value', type='text')
+    ]),
+    html.Br(),
+    html.Div(id='my-output'),
+
+])
+
+
+@app.callback(
+    Output(component_id='my-output', component_property='children'),
+    Input(component_id='my-input', component_property='value')
+)
+def update_output_div(input_value):
+    return f'Output: {input_value}'
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+
+
+'''
 app = Dash(__name__)
 
 df = pd.read_csv('data/compiled_daily_sales_data.csv')
@@ -20,3 +50,4 @@ app.layout = html.Div(children=[
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+    '''
