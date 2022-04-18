7077d42776c76e054f0deb49ec145a68fb6a2fb4
avali4907 <avali4907@outlook.com>
2022-04-18 10:25:28 -0700

    added styling and radio select

--- app.py
+++ app.py
@@ -1,53 +1,69 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
-from dash import Dash, dcc, html
+from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
 import pandas as pd
 
-from dash import Dash, dcc, html, Input, Output
 
 app = Dash(__name__)
 
-app.layout = html.Div([
-    html.H6("Change the value in the text box to see callbacks in action!"),
-    html.Div([
-        "Input: ",
-        dcc.Input(id='my-input', value='initial value', type='text')
-    ]),
-    html.Br(),
-    html.Div(id='my-output'),
-
-])
-
-
-@app.callback(
-    Output(component_id='my-output', component_property='children'),
-    Input(component_id='my-input', component_property='value')
-)
-def update_output_div(input_value):
-    return f'Output: {input_value}'
-
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
-
-
-
-'''
-app = Dash(__name__)
+colors = {
+    'background': '#111111',
+    'line': '#FF1493'
+}
 
 df = pd.read_csv('data/compiled_daily_sales_data.csv')
 
-fig = px.line(df, x="date", y="sales", title="Daily Sales Data from Jan 2018 to present")
+fig = px.line(df, x="date", y="sales", title="Daily Sales Data of Pink Morsel from Jan 2018 to present")
 
 app.layout = html.Div(children=[
-    dcc.Graph(
-        id='pink-sales-graph',
-        figure=fig
-    )
-])
+    html.H1(children='Pink Morsel Sales Data', style={
+        'flex': '0 1 100%',
+    }),
+
+    html.Div(children=[
+        html.Div(children=[
+            html.Div(children='Select a Region:'),
+            dcc.RadioItems(['all', *df['region'].unique()], 'all', id='region-input', inline=True),
+        ], style={
+            'border': '1px solid',
+            'padding': '16px',
+            'width': 'max-content',
+        })
+    ], style={
+        'flex': '0 1 100%',
+    }),
+    
+    html.Div(children= [
+        dcc.Graph(
+            id='pink-sales-graph',
+            figure=fig
+        ),
+    ], style={
+        'flex': '0 1 100%',
+    })
+
+], style={
+    'margin': '16px',
+    'display': 'flex',
+    'flexWrap': 'wrap',
+})
+
+@app.callback(
+    Output('pink-sales-graph', component_property='figure'),
+    Input('region-input', component_property='value')
+)
+def update_figure(region_value):
+    if region_value == 'all':
+        fig = px.line(df, x="date", y="sales", title="Daily Sales Data of Pink Morsel from Jan 2018 to present")
+    else:
+        dff = df[df.region == region_value]
+        fig = px.line(dff, x="date", y="sales", title=f"Daily Sales Data of Pink Morsel from Jan 2018 to present ({region_value})")
+    
+    fig.update_traces(line_color=colors['line'])
+
+    return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-    '''
