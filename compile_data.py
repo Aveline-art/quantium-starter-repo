c4c2f8930b6929b56d4078a4b6fe28f496b39b9a
Ava Li <62368440+Aveline-art@users.noreply.github.com>
2022-04-18 10:27:58 -0700

    Added radio buttons for regions and styling (#3)
    * added dash visualization
    
    * testing tutorial sample
    
    * corrected compiled data
    
    * added styling and radio select

--- compile_data.py
+++ compile_data.py
@@ -4,26 +4,27 @@ import sys
 
 def main(argv):
     folder = argv[0]
-    for file in os.listdir(folder):
-        open_csv(f'{folder}/{file}')
+    paths = [f'{folder}/{file}' for file in os.listdir(folder)]
+    open_csv(paths)
     
 
-def open_csv(file):
-    with open(file, 'r', encoding='utf-8') as f:
-        filereader = csv.DictReader(f)
-
-        with open('data/compiled_daily_sales_data.csv', 'a', newline='', encoding='utf-8') as f:
-            fieldnames = ['product', 'sales', 'date', 'region']
-            writer = csv.DictWriter(f, fieldnames=fieldnames)
-            writer.writeheader()
-            for row in filereader:
-                if row['product'] == 'pink morsel':
-                    writer.writerow({
-                        'product': row['product'],
-                        'sales': int(row['quantity']) * float(row['price'][1:]),
-                        'date': row['date'],
-                        'region': row['region']
-                    })
+def open_csv(paths):
+    with open('data/compiled_daily_sales_data.csv', 'w', newline='', encoding='utf-8') as f:
+        fieldnames = ['product', 'sales', 'date', 'region']
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        
+        for file in paths:
+            with open(file, 'r', encoding='utf-8') as f:
+                filereader = csv.DictReader(f)
+                for row in filereader:
+                    if row['product'] == 'pink morsel':
+                        writer.writerow({
+                            'product': row['product'],
+                            'sales': int(row['quantity']) * float(row['price'][1:]),
+                            'date': row['date'],
+                            'region': row['region']
+                        })
 
 if __name__ == "__main__":
    main(sys.argv[1:])
